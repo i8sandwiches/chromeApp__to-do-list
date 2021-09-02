5c307a9124aaf0ad0502341902554c813d92e3c3
i8sandwiches <61651772+i8sandwiches@users.noreply.github.com>
2021-09-03 00:58:33 +0900

    cursor be smaller on (.be__small-cursor)

--- document/js/cursor.js
+++ document/js/cursor.js
@@ -1,9 +1,23 @@
 const mosueCursor = document.querySelector(".cursor");
+const beSmallCursor = document.querySelector(".be__small-cursor")
+
+const BE_SMALL_CURSOR = "be__small-cursor";
+const GET_SMALL="small-cursor";
 
 window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
 
 function cursor(e) {
     mosueCursor.style.left = e.pageX + "px";
     mosueCursor.style.top = e.pageY + "px";
     mosueCursor.style.display = "block"
+}
+
+function activeCursor(e) {
+    const  item=e.target;
+    if(item.classList.contains(BE_SMALL_CURSOR)){
+        mosueCursor.classList.add(GET_SMALL);
+    } else {
+        mosueCursor.classList.remove(GET_SMALL);
+    }
 }
