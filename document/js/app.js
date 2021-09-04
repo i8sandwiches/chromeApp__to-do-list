03b76a6569d1590e6bf31f5be4b766032994a31d
i8sandwiches <61651772+i8sandwiches@users.noreply.github.com>
2021-09-05 06:18:22 +0900

    make main base

--- document/js/app.js
+++ document/js/app.js
@@ -1,8 +1,9 @@
 const mainButton = document.querySelector(".button");
 const logo = document.querySelector("#logo");
-const hiddenLogo = document.querySelector("#hidden_logo");
+const main= document.querySelector(".main");
 
 const BE_MAIN = "be-main";
+const HIDDEN = "hidden";
 
 mainButton.addEventListener("click", toBeMain);
 logo.addEventListener("click", toBeButton);
@@ -11,9 +12,11 @@ function toBeMain(){
     mainButton.classList.remove("button__hover");
     mainButton.classList.add(BE_MAIN);
     logo.setAttribute("style","color: #131313; font-weight:600;");
+    main.classList.remove(HIDDEN);
 }
 function toBeButton(){
     mainButton.classList.remove(BE_MAIN);
+    mainButton.classList.add("button__hover")
     logo.removeAttribute("style","color: #131313; font-weight:600;");
-    mainButton.classList.add("button__hover");
+    main.classList.add(HIDDEN);
 }
