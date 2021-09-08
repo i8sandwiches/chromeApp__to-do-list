4f14182a9aada71f511f2d7b30ecae18887f5e76
i8sandwiches <61651772+i8sandwiches@users.noreply.github.com>
2021-09-08 14:09:47 +0900

    logo 80%

--- document/js/app.js
+++ document/js/app.js
@@ -11,32 +11,40 @@ mainButton.addEventListener("click", toBeMain);
 logo.addEventListener("click", toBeButton);
 
 function toBeMain(){
+
+    setTimeout(here,1200);
+    function here () {
+        todoContainer.classList.remove(NONE);
+    }
+
     mainButton.classList.remove("button__hover");
     mainButton.classList.add(BE_MAIN);
     logo.setAttribute("style","color: #131313; font-weight:600;");
     main.classList.remove(HIDDEN);
 
-    setTimeout(none,1200);
-    function none () {
-        todoContainer.classList.remove(NONE);
-    }
-
     //if the number of todo is over 10, it can stop make more todo
     const theNumberOfTodo= document.getElementsByClassName("todo").length;
     if (theNumberOfTodo >= 10) {
-        todoButton.setAttribute("style", "pointer-events: none; opacity: 0.1;")
+        todoButton.setAttribute("style", "pointer-events: none; opacity: 0.3;");
+        todoInput.setAttribute("style", "pointer-events:none; opacity: 0.3;");
+        todoInput.placeholder = "max todo list is '10'";
     } else {
-        todoButton.removeAttribute("style", "pointer-events: none; opacity: 0.1;")
+        todoButton.removeAttribute("style", "pointer-events: none; opacity: 0.3;");
+        todoInput.removeAttribute("style", "pointer-events: none; opacity: 0.3;");
+        todoInput.placeholder = "";
     }
 }
 
 function toBeButton(){
+
+    setTimeout(none, 0050);
+    function none () {
+        todoContainer.classList.add(NONE);
+    }
+
     mainButton.classList.remove(BE_MAIN);
     mainButton.classList.add("button__hover")
     logo.removeAttribute("style","color: #131313; font-weight:600;");
     main.classList.add(HIDDEN);
-    todoContainer.classList.add(NONE);
-
 }
-
 
