873a7ab09ce0c1da25f2759784a0a2d8a6b82b03
i8sandwiches <61651772+i8sandwiches@users.noreply.github.com>
2021-09-08 10:04:47 +0900

    99% todo

--- document/js/app.js
+++ document/js/app.js
@@ -1,9 +1,11 @@
 const mainButton = document.querySelector(".button");
 const logo = document.querySelector("#logo");
 const main= document.querySelector(".main");
+const todoContainer= document.querySelector(".right-container__to-do-list");
 
 const BE_MAIN = "be-main";
 const HIDDEN = "hidden";
+const NONE = "none";
 
 mainButton.addEventListener("click", toBeMain);
 logo.addEventListener("click", toBeButton);
@@ -13,10 +15,28 @@ function toBeMain(){
     mainButton.classList.add(BE_MAIN);
     logo.setAttribute("style","color: #131313; font-weight:600;");
     main.classList.remove(HIDDEN);
+
+    setTimeout(none,1200);
+    function none () {
+        todoContainer.classList.remove(NONE);
+    }
+
+    //if the number of todo is over 10, it can stop make more todo
+    const theNumberOfTodo= document.getElementsByClassName("todo").length;
+    if (theNumberOfTodo >= 10) {
+        todoButton.setAttribute("style", "pointer-events: none; opacity: 0.1;")
+    } else {
+        todoButton.removeAttribute("style", "pointer-events: none; opacity: 0.1;")
+    }
 }
+
 function toBeButton(){
     mainButton.classList.remove(BE_MAIN);
     mainButton.classList.add("button__hover")
     logo.removeAttribute("style","color: #131313; font-weight:600;");
     main.classList.add(HIDDEN);
+    todoContainer.classList.add(NONE);
+
 }
+
+
--- document/js/todo.js
+++ document/js/todo.js
@@ -6,41 +6,52 @@ todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 
 function addTodo(event) {
+   //prevent 'F5' effect when you click button 
    event.preventDefault();
 
+   //create div(.todo) into form(.todo_form) 
    const todoDiv = document.createElement("div");
    todoDiv.classList.add("todo");
 
+   //create li(.todo-item) into div(.todo)
    const newTodo = document.createElement("li");
    newTodo.innerText=todoInput.value;
    newTodo.classList.add("todo-item");
    todoDiv.appendChild(newTodo);
-
+   
+   //create button ✔ for check
    const completeButton = document.createElement("button");
    completeButton.innerHTML= "✔";
    completeButton.classList.add("complete-btn");
    todoDiv.appendChild(completeButton);
 
+   //create button ✖ for remove
    const trashButton = document.createElement("button");
    trashButton.innerHTML= "✖";
    trashButton.classList.add("trash-btn");
    todoDiv.appendChild(trashButton);
-
+   
+   //div(.todo) get into todoList 
    todoList.appendChild(todoDiv);
 
+   //it makes your input empty when you add .todo
    todoInput.value ="";
+
 }
 
 function deleteCheck(e) {
     const item=e.target;
-
+    //remove .todo when you click  ✖
     if(item.classList[0]==="trash-btn") {
         const todo = item.parentElement;
         todo.remove();
     }
 
+    //remove .todo when you click  ✔
     if(item.classList[0]==="complete-btn") {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+
+
 }
